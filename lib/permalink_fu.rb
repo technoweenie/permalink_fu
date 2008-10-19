dec57c9682ddd298270e13ef77dad35708eb49ce
cyu <csyu77@gmail.com>
2008-10-19 08:25:40 -0700

    issue where iconv translation of chinese characters to '?????', which comes out blank after subsitutions.  Added failsafe to generate random permalink if the result is ever blank.

--- lib/permalink_fu.rb
+++ lib/permalink_fu.rb
@@ -14,15 +14,20 @@ module PermalinkFu
     def escape(string)
       begin
         result = ((translation_to && translation_from) ? Iconv.iconv(translation_to, translation_from, string) : string).to_s
+        result.gsub!(/[^\x00-\x7F]+/, '') # Remove anything non-ASCII entirely (e.g. diacritics).
+        result.gsub!(/[^\w_ \-]+/i,   '') # Remove unwanted chars.
+        result.gsub!(/[ \-]+/i,      '-') # No more than one of the separator in a row.
+        result.gsub!(/^\-|\-$/i,      '') # Remove leading/trailing separator.
+        result.downcase!
+
+        result.blank? ? random_permalink : result
       rescue
-        result = Digest::SHA1.hexdigest( Time.now.to_s.split(//).sort_by {rand}.join )
+        random_permalink
       end
-      result.gsub!(/[^\x00-\x7F]+/, '') # Remove anything non-ASCII entirely (e.g. diacritics).
-      result.gsub!(/[^\w_ \-]+/i,   '') # Remove unwanted chars.
-      result.gsub!(/[ \-]+/i,      '-') # No more than one of the separator in a row.
-      result.gsub!(/^\-|\-$/i,      '') # Remove leading/trailing separator.
-      result.downcase!
-      result
+    end
+    
+    def random_permalink
+      Digest::SHA1.hexdigest( Time.now.to_s.split(//).sort_by {rand}.join )
     end
   end
 
